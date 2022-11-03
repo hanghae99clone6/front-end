a1ecba62709a17962ee925a382b8407f93dcfeb0
keepfall <kurney@naver.com>
2022-11-03 19:17:47 +0900

    CORS 이슈 해결을 위한 코드 추가

--- src/index.js
+++ src/index.js
@@ -1,11 +1,14 @@
-import React from "react";
-import { CookiesProvider } from "react-cookie";
-import ReactDOM from "react-dom/client";
-import { Provider } from "react-redux";
-import store from "./redux/config/configStore";
-import App from "./App";
+import React from 'react';
+import { CookiesProvider } from 'react-cookie';
+import ReactDOM from 'react-dom/client';
+import { Provider } from 'react-redux';
+import store from './redux/config/configStore';
+import App from './App';
+import axios from 'axios';
 
-const root = ReactDOM.createRoot(document.getElementById("root"));
+axios.defaults.withCredentials = true;
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <CookiesProvider>
     <Provider store={store}>
